eb732e1b2697aa2a7fb2b76b341c0a2ebd9358b7
Carlos Diaz <cdiaz86@gmail.com>
2021-05-04 18:42:39 -0700

    Requires mongoose and routes.

--- app/app.js
+++ app/app.js
@@ -1,7 +1,20 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
+
+const routes = require('./routes');
 
 app.use(express.json());
+app.use(routes);
+
+app.use((err, req, res, next) => {
+  if (err instanceof mongoose.Error.ValidationError) {
+    return res.status(400).send(err);
+  } else {
+    res.status(500).send("Server Error");
+  }
+
+  console.error(err);
+});
 
 module.exports =  app;
