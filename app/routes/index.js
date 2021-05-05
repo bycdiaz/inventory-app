e15e4ec45647120a3d48ececb95995603f8a443b
Carlos Diaz <cdiaz86@gmail.com>
2021-05-04 18:45:47 -0700

    Adds router index file.

---
+++ app/routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const categoryRoutes = require("./categoryRoutes");
+
+router.use('/categories', categoryRoutes);
+
+module.exports = router;
