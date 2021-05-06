e0ec348dcbbfbee6dced40e0783808d60ab75563
Carlos Diaz <cdiaz86@gmail.com>
2021-05-05 17:43:52 -0700

    Adds standard page layout.

---
+++ frontend/src/layouts/Standard.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Styled from "styled-components";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const PageStyle = Styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  .content {
+    flex: 1 0 auto;
+  }
+
+  .footer {
+    flex-shrink: 0;
+  }
+`
+
+function PageWrapper(props) {
+  return (
+    <PageStyle>
+      <div className='content'>
+        <Header title={props.title}/>
+        <Footer className='footer' />
+      </div>
+    </PageStyle>
+  )
+}
+
+export default PageWrapper;
