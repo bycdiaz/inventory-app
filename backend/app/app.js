692872eb5728e24fbd2015f344c8bc7608ac7e75
Carlos Diaz <cdiaz86@gmail.com>
2021-05-05 18:52:11 -0700

    Requires and uses cors.

--- backend/app/app.js
+++ backend/app/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require('cors');
 
 const routes = require('./routes');
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.use((err, req, res, next) => {
