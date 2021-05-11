52406f632e044cb081675b749093c1f1880917d4
Carlos Diaz <cdiaz86@gmail.com>
2021-05-10 20:26:54 -0700

    Renders category with card styling.

--- frontend/src/components/Category.js
+++ frontend/src/components/Category.js
@@ -2,14 +2,30 @@ import React from "react";
 import Styled from "styled-components";
 
 const CategoryContainer = Styled.div`
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 40%;
 
+  :hover {
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+  }
+
+  .info {
+    padding: 5px 16px;
+  }
+
+  .name {
+    font-weight: bold;
+  }
 `
 
 function Category(props) {
   return (
     <CategoryContainer>
-      <p className='name'>{props.name}</p>
-      <p className='descriptipn'>{props.description}</p>
+      <div className="info">
+        <p className="name">{props.name}</p>
+        <p>{props.description}</p> 
+      </div>
     </CategoryContainer>
   )
 }
