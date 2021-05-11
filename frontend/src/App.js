38fcb624e2303af1d52950975be3a32f02a929e1
Carlos Diaz <cdiaz86@gmail.com>
2021-05-10 20:26:31 -0700

    Wraps all components in App with standard layout.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
+import StandardLayout from "./layouts/Standard";
 import Landing from "./pages/Landing";
 import Categories from "./pages/Categories";
 import Items from "./pages/Items";
 
 function App() {
   return (
-     <Switch>
-      <Route path="/categories">
-        <Categories />
-      </Route>
-      <Route path="/items">
-        <Items />
-      </Route>
-      <Route path="/">
-        <Landing />
-      </Route>
-   </Switch>
+    <StandardLayout title={"Inventory App"}>
+      <Switch>
+        <Route path="/categories">
+          <Categories />
+        </Route>
+        <Route path="/items">
+          <Items />
+        </Route>
+        <Route path="/">
+          <Landing />
+        </Route>
+    </Switch>
+   </StandardLayout>
   );
 }
 
