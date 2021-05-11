c6565aa239ef19c477be8609f4fcd9c684d78320
Carlos Diaz <cdiaz86@gmail.com>
2021-05-10 19:17:52 -0700

    Imports React Router, Adds Switch, and imports components for routes.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 
 import Landing from "./pages/Landing";
+import Categories from "./pages/Categories";
+import Items from "./pages/Items";
 
 function App() {
   return (
-    <div className="App">
-      <Landing />
-    </div>
+     <Switch>
+      <Route path="/categories">
+        <Categories />
+      </Route>
+      <Route path="/items">
+        <Items />
+      </Route>
+      <Route path="/">
+        <Landing />
+      </Route>
+   </Switch>
   );
 }
 
