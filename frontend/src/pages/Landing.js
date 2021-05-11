73f0d7ada5dc92be91cb5e9a5c5635618c16090d
Carlos Diaz <cdiaz86@gmail.com>
2021-05-10 20:28:17 -0700

    Removes standard layout from landing component.

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -2,8 +2,6 @@ import React from "react";
 import Styled from "styled-components";
 import { Link } from "react-router-dom";
 
-import StandardLayout from "../layouts/Standard";
-
 const LandingContent = Styled.div`
   display: flex;
   flex-direction: column;
@@ -11,17 +9,15 @@ const LandingContent = Styled.div`
 `
 
 function Landing() {
-  return (
-    <StandardLayout title={"Inventory App"}>
-      <LandingContent>
-        <Link to="/categories">
-          <button>All Categories</button>   
-        </Link>
-        <Link to="/items">
-          <button>All Items</button>
-        </Link>
-      </LandingContent>
-    </StandardLayout>
+  return (    
+    <LandingContent>
+      <Link to="/categories">
+        <button>All Categories</button>   
+      </Link>
+      <Link to="/items">
+        <button>All Items</button>
+      </Link>
+    </LandingContent>
   )
 }
 
