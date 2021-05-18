1ca7de2accc9443e54344dfb3f98a7371827a5a2
Carlos Diaz <cdiaz86@gmail.com>
2021-05-17 18:33:06 -0700

    Adds allitems route for router switch.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,26 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import StandardLayout from "./layouts/Standard";
-import Landing from "./pages/Landing";
-import Categories from "./pages/Categories";
-import CategoryItems from "./pages/CategoryItems";
+import StandardLayout from './layouts/Standard';
+import Landing from './pages/Landing';
+import Categories from './pages/Categories';
+import CategoryItems from './pages/CategoryItems';
+import AllItems from './pages/AllItems';
 
 function App() {
   return (
-    <StandardLayout title={"Inventory App"}>
+    <StandardLayout title={'Inventory App'}>
       <Switch>
-        <Route path="/categories/:category">
+        <Route path='/allitems'>
+            <AllItems />
+        </Route>
+        <Route path='/categories/:category'>
           <CategoryItems />
         </Route>
-        <Route path="/categories">
+        <Route path='/categories'>
           <Categories />
         </Route>
-        <Route path="/">
+        <Route path='/'>
           <Landing />
         </Route>
     </Switch>
