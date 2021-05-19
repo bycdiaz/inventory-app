7adde2915ca641d0b7c67040a766035cbccd2d5e
Carlos Diaz <cdiaz86@gmail.com>
2021-05-18 18:54:50 -0700

    Adds method to retrieve items by category.

--- backend/app/controllers/categoryController.js
+++ backend/app/controllers/categoryController.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose');
-const Category = mongoose.model("Category");
+const Category = mongoose.model('Category');
+const Item = mongoose.model('Item');
 
 exports.getCategories = async (req, res, next) => {
   try {
     const categories = await Category.find();
     return res.json(categories);
+  } catch (error) {
+    next(error);
+  }
+}
+
+exports.itemsByCategory = async (req, res, next) => {
+  const categoryRequested = req.params.category.replace(/^\w/, (c) => c.toUpperCase());
+
+  try {
+    const { _id } = await Category.findOne({ name: categoryRequested });
+    const itemsByCategory = await Item.find({ category: _id });
+  
+    return res.json(itemsByCategory);
   } catch (error) {
     next(error);
   }
