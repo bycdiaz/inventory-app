2d65840c0ebe9b97f4eea318b3ee33ee3528a0c5
Carlos Diaz <cdiaz86@gmail.com>
2021-05-18 20:59:39 -0700

    Renders list of retrieved items.

--- frontend/src/pages/CategoryItems.js
+++ frontend/src/pages/CategoryItems.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from "react";
-import Styled from "styled-components";
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import Styled from 'styled-components';
+import { useParams } from 'react-router-dom';
+
+import ItemCard from '../components/ItemCard';
 
 
 const ItemsContent = Styled.div`
@@ -11,30 +13,42 @@ const ItemsContent = Styled.div`
 
 function CategoryItems() {
   const { category } = useParams();
-
   const [items, setItems] = useState([]);
-  console.log(items);
-  async function getItemsByCategory() {
-    const response = await fetch(`http://localhost:3000/categories/${category}`);
-    const items = await response.json();
 
-    return setItems(items);
+  useEffect(() => {
+    let isMounted = true;
+    
+    async function getItemsByCategory() {
+      const response = await fetch(`http://localhost:3000/categories/${category}`);
+      const items = await response.json();
+  
+      return setItems(items);
+    }
+
+    if (isMounted) getItemsByCategory();
+
+    return () => {
+      isMounted = false;
+    }
+  },[category]);
+
+  function Items() {
+    return (
+      <ul>
+        { items.map(item =>  {
+          return (
+            <li key={item._id}>
+              <ItemCard item={item} />
+            </li>
+          )
+        })}
+      </ul>
+    )
   }
 
-    useEffect(() => {
-      let isMounted = true;
-
-      if (isMounted) getItemsByCategory();
-
-      return () => {
-        isMounted = false;
-      }
-    }, []);
-  
   return (
     <ItemsContent>
-      {/* TODO Render all items */}
-      <p>{`All items for the ${category} category`}</p>
+      <Items />
     </ItemsContent>
   )
 }
