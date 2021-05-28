2c81fb83f598cf71be9ef7c965e1a073b3fcff44
Carlos Diaz <cdiaz86@gmail.com>
2021-05-28 14:19:59 -0700

    Renders item details. Adds initial styling.

--- frontend/src/pages/Item.js
+++ frontend/src/pages/Item.js
@@ -1,6 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 
+import Styled from "styled-components";
+
+const ItemContainer = Styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #ebc340;
+  height: 100%;
+  width: 60%;
+  margin-top: 30px;
+  padding: 20px;
+
+  .page-title {
+    font-size: 18pt;
+    font-weight: bold;
+    text-align: center;
+    margin-top: 20px;
+  }
+
+  .details {
+    width: 80%;
+  }
+
+  .edit-item {
+    padding: 10px 0px;
+    width: 40%;
+  }
+
+  .item-detail {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 10px 0px;
+  }
+`
 
 function Item() {
   const { itemId } = useParams();
@@ -23,14 +59,37 @@ function Item() {
     }
   },[itemId]);
 
+  function ItemDetails() {
+    const { categoryName, description, name, price, stock } = item;
+    const detailsToDisplay = {
+      category: categoryName,
+      description,
+      name,
+      price,
+      stock
+    }
+    console.log(detailsToDisplay);
+
+    return Object.keys(detailsToDisplay).map(detailName => {
+      return (
+        <div className='item-detail' key={name}>
+          <p>{detailName.charAt(0).toUpperCase() + detailName.slice(1)}</p>
+          <p>{detailsToDisplay[detailName]}</p>
+        </div>
+      )
+    })
+  }
+
+  ItemDetails()
+
   return (
-    <>
-      <p>{item.description}</p>
-      <p>{item.name}</p>
-      <p>{item.price}</p>
-      <p>{item.stock}</p>
-      <p>{item.categoryName}</p>
-    </>
+    <ItemContainer>
+      <h1 className='page-title'>Item Details</h1>
+      <div className='details'>
+        <ItemDetails />
+      </div>
+      <button className='edit-item'>Edit This Item</button>
+    </ItemContainer>
   )
 }
 
