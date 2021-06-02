e8ccda9f1b7ae7fe3f2cef63a4253d37e7e4d880
Carlos Diaz <cdiaz86@gmail.com>
2021-06-01 17:58:32 -0700

    Creates initial edit item component.

---
+++ frontend/src/pages/EditItem.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+import Styled from "styled-components";
+
+import ItemForm from '../components/ItemForm';
+
+const EditItemContainer = Styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #ebc340;
+  height: 100%;
+  width: 60%;
+  margin-top: 30px;
+  padding: 20px;
+`
+
+function EditItem(props) {
+  const { itemId } = useParams();
+  const [ item, setItem ] = useState([]);
+
+  useEffect(() => {
+    let isMounted = true;
+    
+    async function getItemById() {
+      const response = await fetch(`http://localhost:3000/items/${itemId}`);
+      const item = await response.json();
+
+      return setItem(item);
+    }
+
+    if (isMounted) getItemById();
+
+    return () => {
+      isMounted = false;
+    }
+  },[itemId]);
+
+  return (
+    <EditItemContainer>
+      <ItemForm item={item} />
+    </EditItemContainer>
+  )
+}
+
+export default EditItem;
