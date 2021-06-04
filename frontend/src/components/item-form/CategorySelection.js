76a98b895e8824dbb70eb5f2d2fc1fbeea3b8d21
Carlos Diaz <cdiaz86@gmail.com>
2021-06-03 18:23:20 -0700

    Adds Category selection component.

---
+++ frontend/src/components/item-form/CategorySelection.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function CategorySelection(props) {
+  const filteredCategories = props.categories.filter(category => category.name !== props.categoryName);
+
+  return (
+    <>
+      <p>Current Category - {props.categoryName}</p>
+      <label htmlFor='category'>New Category:</label>
+      <select name='category' onChange={props.handleChange} defaultValue='-- select an option --'>
+      <option disabled> -- select an option -- </option>
+        {filteredCategories.map(category => {
+          return (
+            <option
+              key={category._id}
+              value={category._id}
+            >
+              { category.name }
+            </option>
+          )
+        })}
+      </select>
+    </>
+  )
+}
+
+export default CategorySelection;
