2299caf28df6c9020b2320f89d3463a278d78e77
Carlos Diaz <cdiaz86@gmail.com>
2021-06-05 12:55:33 -0700

    Disables button after delete request is sent.

--- frontend/src/components/DeleteButton.js
+++ frontend/src/components/DeleteButton.js
@@ -5,18 +5,29 @@ const Button = Styled.button`
   margin-top: 30px;
   color: white;
   background-color: red;
+  padding: 5px 10px;
 `
 
 function DeleteButton() {
   const [clickCount, setClickCount] = useState(0);
 
   function handleClick() {
-    const newCount = clickCount + 1;
-    setClickCount(newCount);
+    // disable button after delete request is sent.
+    if (clickCount === 0) {
+      const newCount = clickCount + 1;
+      setClickCount(newCount);
+    } else if (clickCount === 1) {
+      console.log('api call for deleting just fired');
+      const newCount = clickCount + 1;
+      setClickCount(newCount);
+    }  
   }
 
   return (
-    <Button onClick={handleClick}>Delete Item</Button>
+      <Button onClick={handleClick} disabled={clickCount === 2}>
+        {clickCount === 0 ? 'Delete Item?' : 'Confirm Delete'}
+      </Button>
+      // TODO show delete success or failure
   )
 }
 
