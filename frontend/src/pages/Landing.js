7a70e01ca19607b4790b963fcbb9779792eb7c62
Carlos Diaz <cdiaz86@gmail.com>
2021-06-07 19:50:07 -0700

    Hides delete message with CSS animation.

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useLocation } from 'react-router-dom';
 import Styled from "styled-components";
 
 const LandingContent = Styled.div`
@@ -7,12 +8,36 @@ const LandingContent = Styled.div`
   align-items: center;
   margin-top: 60px;
   font-size: 20pt;
+
+  .delete-message {
+    font-size: 14pt;
+    font-weight: bold;
+    color: green;
+    -webkit-animation: fadeAnimation 3s forwards; 
+    animation: fadeAnimation 3s forwards;
+    margin-bottom: 30px;
+  }
+
+  @keyframes fadeAnimation {
+    0%   {opacity: 1;}
+    90%  {opacity: 1;}
+    100% {opacity: 0;}
+  }
+
+  @-webkit-keyframes fadeAnimation {
+      0%   {opacity: 1;}
+      90%  {opacity: 1;}
+      100% {opacity: 0;}
+  }
 `
 
 function Landing() {
+  const { state } = useLocation();
+
   return (
     <LandingContent>
-      Choose an action above.
+      { state ? <p className='delete-message'>Item Deleted!</p> : null }
+      <p>Choose an action above.</p>
     </LandingContent>
   )
 }
