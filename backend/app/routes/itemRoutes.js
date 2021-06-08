5a406f664dd2140d12185f7cc6b2b6fd9b93a15d
Carlos Diaz <cdiaz86@gmail.com>
2021-06-07 19:47:28 -0700

    Adds delete route.

--- backend/app/routes/itemRoutes.js
+++ backend/app/routes/itemRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const item = require('../controllers/itemController');
 
-router.get("/:id", item.byId);
-router.get("/", item.allItems);
+router.get('/:id', item.byId);
+router.get('/', item.allItems);
 
-router.post("/update/:_id", item.updateItem);
+router.post('/update/:_id', item.updateItem);
+
+router.delete('/delete/:_id', item.deleteItem);
 
 module.exports = router;
