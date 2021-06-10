be432bacec83da64b46092a74bca1260e2843454
Carlos Diaz <cdiaz86@gmail.com>
2021-06-09 18:41:53 -0700

    Adds create item page.

---
+++ frontend/src/pages/CreateItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Styled from "styled-components";
+
+import ItemForm from '../components/item-form/ItemForm';
+
+const CreateItemContainer = Styled.div`
+  margin-top: 60px;
+`
+
+function CreateItem() {
+  const newItem = {
+    name: '',
+    categoryName: '',
+    description: '',
+    price: 0,
+    stock: 0
+  }
+  return (
+    <CreateItemContainer>
+      <ItemForm item={newItem} />
+    </CreateItemContainer>
+  );
+}
+
+export default CreateItem;
