bf83549396f2f241022205492cbe57972d8260ea
Carlos Diaz <bycdiaz@protonmail.com>
2021-06-15 16:13:04 -0700

    Adds put endpoint for updating category.

--- backend/app/routes/categoryRoutes.js
+++ backend/app/routes/categoryRoutes.js
@@ -5,6 +5,7 @@ const category = require('../controllers/categoryController');
 
 
 router.get("/", category.getCategories);
+router.put("/:category/edit", category.updateCategory);
 router.get("/:category", category.itemsByCategory);
 
 module.exports = router;
